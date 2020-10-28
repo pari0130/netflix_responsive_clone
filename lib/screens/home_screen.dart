f1a71b79cfc4314de67612572cf4d60b7517982b
donghwi <pari0130@gmail.com>
2020-10-28 21:58:19 +0900

    study 2020-10-28

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:netflix/widgets/widgets.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  ScrollController _scrollController;
+  double _scrollOffset = 0.0;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController()
+      ..addListener(() {
+        setState(() {
+          _scrollOffset = _scrollController.offset;
+        });
+      });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.grey[850],
-          child: const Icon(Icons.cast),
-          onPressed: () => print('Cast'),
-        ),
-        appBar: PreferredSize(
-          preferredSize: Size(screenSize.width, 50.0),
-          child: CustomAppBar(),
-        ));
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey[850],
+        child: const Icon(Icons.cast),
+        onPressed: () => print('Cast'),
+      ),
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 50.0),
+        child: CustomAppBar(),
+      ),
+      body: CustomScrollView(
+        controller: _scrollController,
+      ),
+    );
   }
 }
